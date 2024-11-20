ea20a20ac9a81136e7e010d9ee59cd5fc9c324b7
painghtetaung <painghtetaung1999@gmail.com>
2024-11-20 16:12:16 +0700

    changed cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ server.listen(process.env.PORT || 8000, () =>
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "*"],
+    origin: ["*", "http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
